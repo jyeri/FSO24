c90b0418780be2832c9e3ed894f237fe855910e3
jrummuka <jrummuka@student.hive.fi>
2025-03-21 21:26:19 +0200

    2.10-2.15

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,19 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect, use } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import personService from './services/persons'
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+  
+
+  useEffect(() => {
+    console.log('effect')
+    personService
+      .getAll()
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+    }, [])
+  
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -23,16 +32,41 @@ const App = () => {
     }
     console.log(personObject)
     if (persons.some(person => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`)
-    }
-    if (persons.some(person => person.number === newNumber)) {
-      alert(`${newNumber} is already added to phonebook`)
-    }
-    else {
-      setPersons(persons.concat(personObject))
+      if (persons.some(person => person.number !== newNumber)) {
+        if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`) === false) {
+          return
+        }
+        else {
+          personService
+            .update(persons.find(person => person.name === newName).id, personObject)
+            .then(response => {
+              console.log(response)
+              setPersons(persons.map(person => person.name === newName ? response.data : person))
+            })
+        }
+      }
+    } else {
+      personService
+        .create(personObject)
+        .then(response => {
+          console.log(response)
+          setPersons(persons.concat(response.data))
+        })
       setNewName('')
       setNewNumber('')
     }
+  }
+
+  const handleDelete = (name, id) => {
+    if (window.confirm(`Delete ${name}?`) === false) {
+      return
+    }
+    personService
+      .deletePerson(id)
+      .then(response => {
+        console.log(response)
+        setPersons(persons.filter(person => person.id !== id))
+      })
   }
 
   const handleInputChange = (event) => {
@@ -54,7 +88,7 @@ const App = () => {
       <h2>Phonebook</h2>
       <Filter text="filter with" value={newFilter} handleFilterChange={handleFilterChange}/>
       <PersonForm text="Add a New" handleSubmit={handleSubmit} newName={newName} newNumber={newNumber} handleInputChange={handleInputChange} handleNumberChange={handleNumberChange}/>
-      <Persons text="numbers" persons={persons} newFilter={newFilter}/>
+      <Persons text="numbers" persons={persons} newFilter={newFilter} handleDelete={handleDelete}/>
     </div>
   )
 
--- osa2/puhelinluettelo/src/components/Persons.jsx
+++ osa2/puhelinluettelo/src/components/Persons.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Persons = ({ text, persons, newFilter }) => {
+const Persons = ({ text, persons, newFilter, handleDelete }) => {
     return (
       <div>
         <h2>{text}</h2>
@@ -8,7 +8,10 @@ const Persons = ({ text, persons, newFilter }) => {
           {persons
             .filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
             .map((person, index) => (
+            <div key={index}>
               <li key={index}>{person.name} - {person.number}</li>
+              <button onClick={() => handleDelete(person.name, person.id)}>delete</button>
+            </div>
             ))}
         </ul>
       </div>
